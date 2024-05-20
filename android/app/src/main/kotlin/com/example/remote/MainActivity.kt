dc19bde4e0ad22c58d77daab1281f4460b4231db
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-05-20 18:28:13 +0530

    added ss

--- android/app/src/main/kotlin/com/example/remote/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/remote/MainActivity.kt
@@ -1,10 +1,5 @@
 package com.example.remote
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-import dev.darttools.flutter_android_volume_keydown.FlutterAndroidVolumeKeydownActivity
 
-class MainActivity : FlutterAndroidVolumeKeydownActivity() {}
-
+class MainActivity: FlutterActivity()
